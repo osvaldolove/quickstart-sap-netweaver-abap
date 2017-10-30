28ee1251699d3418246a78631fcedbbbd493f090
Somckit Khemmanivanh <somckitk@amazon.com>
2017-10-30 10:14:49 -0700

    updated download script logging for failures

--- scripts/download-sapapp.sh
+++ scripts/download-sapapp.sh
@@ -15,6 +15,7 @@ then
 	echo "Download of /root/install/sap-app-pas-install-single-hosts.sh file not successfull"
 	echo "exiting..."
 	mv /var/run/dbus/system_bus_socket.bak /var/run/dbus/system_bus_socket 
+	 /root/install/signalFinalStatus.sh 1 "Download-script not found...check: /root/install/sap-app-pas-install-single-hosts.sh"
 	echo 1
 	exit 1
 else
@@ -23,6 +24,7 @@ else
 	bash -x /root/install/sap-app-pas-install-single-hosts.sh | tee -a /root/install/sap-app-pas-install-single-hosts-out.log
 	if [ $? -ne 0 ] 
 	then
+	 	/root/install/signalFinalStatus.sh 1 "Install-script did not execute correctly...check: /var/log directories for error message"
 		echo 1
 		exit 1
 	else
